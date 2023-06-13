29e5121d8415c33f8bd50552494f06758f9027b6
Emil Melnikov <emilmelnikov@users.noreply.github.com>
2023-06-13 18:26:54 +0200

    Add ZIP64 support (#2)

--- src/netzip.py
+++ src/netzip.py
@@ -6,15 +6,15 @@ for the ZIP file format specification.
 
 import collections.abc
 import io
+import itertools
 import struct
 import zlib
 from dataclasses import dataclass
-from typing import ClassVar, Dict, Iterable, Tuple
+from typing import Any, ClassVar, Dict, Iterable, Tuple
 
 _MULTI_DISK_ERROR = ValueError("multi-disk ZIP archives are not supported")
 _ENCRYPTED_ERROR = ValueError("encrypted ZIP archives are not supported")
 _COMPRESSED_ERROR = ValueError("compressed ZIP archives are not supported")
-_ZIP64_ERROR = NotImplementedError("ZIP64 archives are not implemented yet")
 
 
 def _validate_signature(name: str, *, want: bytes, got: bytes) -> None:
@@ -23,18 +23,15 @@ def _validate_signature(name: str, *, want: bytes, got: bytes) -> None:
         raise ValueError(f"invalid {name} signature: want {want}, got {got}")
 
 
-def _fullread(buf, size: int = -1) -> bytes:
-    """Read exactly `size` bytes from `buf`, or raise EOFError.
-
-    If size is negative, read until EOF.
-    """
+def _fullread(buf, size: int) -> bytes:
+    """Read exactly `size` bytes from `buf`, or raise EOFError."""
     data = buf.read(size)
-    if size >= 0 and len(data) != size:
+    if len(data) != size:
         raise EOFError(f"requested {size} bytes, got {len(data)}")
     return data
 
 
-def _unpackbuf(format, buf) -> Tuple:
+def _unpackbuf(format: str, buf) -> Tuple:
     """Unpack a struct from buffer.
 
     Read the required number of bytes from `buf` and unpack them according to `format`.
@@ -45,8 +42,17 @@ def _unpackbuf(format, buf) -> Tuple:
 # region File format data structures
 
 
-## Fields in the following data structures are separated into two groups:
-## fixed-size and variable-size ones.
+# The following data structures are defined in the ZIP file format specification.
+
+# Some fields are omitted because they are not used by this implementation, and parsing
+# them would add extra I/O, which is undesirable for network access.
+
+# Each data structure has a `read` classmethod that reads the structure from a buffer.
+# The `validate` method checks that the structure is valid and can be used by this
+# implementation.
+
+# Fields in all data structures have default values because they cannot be created
+# in a single step: almost all structures contain some variable-length fields.
 
 
 @dataclass
@@ -70,7 +76,6 @@ class _EocdRecord:
 
     @classmethod
     def read(cls, buf) -> "_EocdRecord":
-        """Read an end of central directory record from `buf`."""
         inst = cls(*_unpackbuf(cls.STRUCT_FORMAT, buf))
         inst.comment = _fullread(buf, inst.comment_size)
         inst.validate()
@@ -78,7 +83,7 @@ class _EocdRecord:
 
     def validate(self) -> None:
         _validate_signature(
-            "end of central directory", want=self.SIGNATURE, got=self.signature
+            "end of central directory record", want=self.SIGNATURE, got=self.signature
         )
         if (
             self.this_disk_number != 0
@@ -86,12 +91,119 @@ class _EocdRecord:
             or self.this_record_count != self.total_record_count
         ):
             raise _MULTI_DISK_ERROR
-        if (
-            self.total_record_count == 0xFFFF
+
+    def is_zip64(self) -> bool:
+        return (
+            self.this_disk_number == 0xFFFF
+            or self.start_disk_number == 0xFFFF
+            or self.this_record_count == 0xFFFF
+            or self.total_record_count == 0xFFFF
             or self.central_directory_size == 0xFFFFFFFF
             or self.central_directory_offset == 0xFFFFFFFF
+        )
+
+
+@dataclass
+class _Zip64EocdRecord:
+    """End of central directory record for ZIP64 archives."""
+
+    signature: bytes = b""
+    size: int = 0
+    version_created: int = 0
+    version_needed: int = 0
+    this_disk_number: int = 0
+    start_disk_number: int = 0
+    this_record_count: int = 0
+    total_record_count: int = 0
+    central_directory_size: int = 0
+    central_directory_offset: int = 0
+
+    SIGNATURE: ClassVar[bytes] = b"PK\x06\x06"
+    STRUCT_FORMAT: ClassVar[str] = "<4sQ2H2L4Q"
+    SIZE: ClassVar[int] = struct.calcsize(STRUCT_FORMAT)
+
+    @classmethod
+    def read(cls, buf) -> "_Zip64EocdRecord":
+        inst = cls(*_unpackbuf(cls.STRUCT_FORMAT, buf))
+        inst.validate()
+        return inst
+
+    def validate(self) -> None:
+        _validate_signature(
+            "ZIP64 end of central directory record",
+            want=self.SIGNATURE,
+            got=self.signature,
+        )
+        if (
+            self.this_disk_number != 0
+            or self.start_disk_number != 0
+            or self.this_record_count != self.total_record_count
         ):
-            raise _ZIP64_ERROR
+            raise _MULTI_DISK_ERROR
+
+
+@dataclass
+class _Zip64EocdLocator:
+    """Location of the ZIP64 end of central directory."""
+
+    signature: bytes = b""
+    eocd_disk_number: int = 0
+    eocd_offset: int = 0
+    total_disk_count: int = 0
+
+    SIGNATURE: ClassVar[bytes] = b"PK\x06\x07"
+    STRUCT_FORMAT: ClassVar[str] = "<4sLQL"
+    SIZE: ClassVar[int] = struct.calcsize(STRUCT_FORMAT)
+
+    @classmethod
+    def read(cls, buf) -> "_Zip64EocdLocator":
+        inst = cls(*_unpackbuf(cls.STRUCT_FORMAT, buf))
+        inst.validate()
+        return inst
+
+    def validate(self) -> None:
+        _validate_signature(
+            "ZIP64 end of central directory locator",
+            want=self.SIGNATURE,
+            got=self.signature,
+        )
+        if self.eocd_disk_number != 0 or self.total_disk_count != 1:
+            raise _MULTI_DISK_ERROR
+
+
+def _read_zip64_extinfo(block) -> Dict[str, Any]:
+    """Read ZIP64 extended information extra field from the 'extra' field block.
+
+    Return dict with values found in the extra block, and keys corresponding
+    to field names in _CdFileHeader and _LocalFileHeader.
+    """
+    buf = io.BytesIO(block)
+
+    while buf.tell() < len(block):
+        header, size = _unpackbuf("<2H", buf)
+        if header != 0x0001:
+            buf.seek(size, io.SEEK_CUR)
+            continue
+
+        formats = dict(
+            uncompressed_size="<Q",
+            compressed_size="<Q",
+            local_header_offset="<Q",
+            disk_number="<L",
+        )
+        if size not in itertools.accumulate(map(struct.calcsize, formats.values())):
+            raise ValueError("invalid ZIP64 extended information extra field")
+
+        fields = {}
+        ext_block = _fullread(buf, size)
+        ext_buf = io.BytesIO(ext_block)
+        for name, fmt in formats.items():
+            if ext_buf.tell() >= len(ext_block):
+                break
+            fields[name] = _unpackbuf(fmt, ext_buf)[0]
+        return fields
+
+    return {}
 
 
 @dataclass
@@ -126,14 +238,17 @@ class _CdFileHeader:
     def read(cls, buf) -> "_CdFileHeader":
         inst = cls(*_unpackbuf(cls.STRUCT_FORMAT, buf))
         inst.filename = _fullread(buf, inst.filename_size)
-        _fullread(buf, inst.extra_size)
+        extra_block = _fullread(buf, inst.extra_size)
         inst.comment = _fullread(buf, inst.comment_size)
+        if inst.is_zip64():
+            for name, value in _read_zip64_extinfo(extra_block).items():
+                setattr(inst, name, value)
         inst.validate()
         return inst
 
     def validate(self) -> None:
         _validate_signature(
-            "central directory ", want=self.SIGNATURE, got=self.signature
+            "central directory file header", want=self.SIGNATURE, got=self.signature
         )
         if self.flags & 0x0001:
             raise _ENCRYPTED_ERROR
@@ -144,11 +259,14 @@ class _CdFileHeader:
             raise _COMPRESSED_ERROR
         if self.disk_number != 0:
             raise _MULTI_DISK_ERROR
-        if (
-            self.uncompressed_size == 0xFFFFFFFF
+
+    def is_zip64(self) -> bool:
+        return (
+            self.compressed_size == 0xFFFFFFFF
+            or self.uncompressed_size == 0xFFFFFFFF
+            or self.disk_number == 0xFFFF
             or self.local_header_offset == 0xFFFFFFFF
-        ):
-            raise _ZIP64_ERROR
+        )
 
 
 @dataclass
@@ -177,7 +295,10 @@ class _LocalFileHeader:
     def read(cls, buf) -> "_LocalFileHeader":
         inst = cls(*_unpackbuf(cls.STRUCT_FORMAT, buf))
         inst.filename = _fullread(buf, inst.filename_size)
-        _fullread(buf, inst.extra_size)
+        extra_block = _fullread(buf, inst.extra_size)
+        if inst.is_zip64():
+            for name, value in _read_zip64_extinfo(extra_block).items():
+                setattr(inst, name, value)
         inst.validate()
         return inst
 
@@ -192,8 +313,11 @@ class _LocalFileHeader:
             or self.compressed_size != self.uncompressed_size
         ):
             raise _COMPRESSED_ERROR
-        if self.uncompressed_size == 0xFFFFFFFF:
-            raise _ZIP64_ERROR
+
+    def is_zip64(self) -> bool:
+        return (
+            self.compressed_size == 0xFFFFFFFF or self.uncompressed_size == 0xFFFFFFFF
+        )
 
 
 # endregion
@@ -234,17 +358,10 @@ class _FileInfo:
 
     def validate_header(self, header: _LocalFileHeader) -> None:
         """Validate against local file header."""
-        if self.size != header.uncompressed_size:
-            raise ValueError("size mismatch")
-        if self.crc32 != header.crc32:
-            raise ValueError("CRC32 mismatch")
-
-    def validate_payload(self, payload: bytes) -> None:
-        """Validate against file payload."""
-        if self.size != len(payload):
-            raise ValueError("size mismatch")
-        if self.crc32 != zlib.crc32(payload):
-            raise ValueError("CRC32 mismatch")
+        if self.size != header.uncompressed_size or self.crc32 != header.crc32:
+            raise ValueError(
+                "local file header and central directory file header contain different metadata"
+            )
 
 
 def _read_metadata(source: io.BufferedIOBase) -> Dict[bytes, _FileInfo]:
@@ -253,13 +370,24 @@ def _read_metadata(source: io.BufferedIOBase) -> Dict[bytes, _FileInfo]:
     Returns:
         Mapping of file names to file info records.
     """
-    source.seek(-_EocdRecord.MAX_SIZE, io.SEEK_END)
+    source.seek(0, io.SEEK_END)
+    size = source.tell()
+
+    source.seek(max(0, size - _EocdRecord.MAX_SIZE - _Zip64EocdLocator.SIZE))
     eocd_block = source.read()
 
     eocd_start = eocd_block.rfind(_EocdRecord.SIGNATURE)
     if eocd_start < 0:
         raise ValueError("end of central directory record not found")
     eocd = _EocdRecord.read(io.BytesIO(eocd_block[eocd_start:]))
+
+    if eocd.is_zip64():
+        loc_block = eocd_block[max(0, eocd_start - _Zip64EocdLocator.SIZE) : eocd_start]
+        loc = _Zip64EocdLocator.read(io.BytesIO(loc_block))
+
+        source.seek(loc.eocd_offset)
+        eocd64_block = source.read(_Zip64EocdRecord.SIZE)
+        eocd = _Zip64EocdRecord.read(io.BytesIO(eocd64_block))
 
     source.seek(eocd.central_directory_offset)
     cd_block = source.read(eocd.central_directory_size)
@@ -287,8 +415,9 @@ class ZipReader(collections.abc.Mapping):
         buf = io.BytesIO(self._source.read(info.max_block_size))
 
         info.validate_header(_LocalFileHeader.read(buf))
-        data = buf.read(info.size)
-        info.validate_payload(data)
+        data = _fullread(buf, info.size)
+        if info.crc32 != zlib.crc32(data):
+            raise ValueError("CRC32 mismatch")
         return data
 
     def __iter__(self) -> Iterable[bytes]:
